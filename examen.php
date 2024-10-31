5ac3e927982752fbcecb178aaa6dc9347ed8f580
HTP <htp@htp.es>
2024-10-31 09:07:49 +0000

    Segundo commit

--- examen.php
+++ examen.php
@@ -1,2 +1,5 @@
-<?php echo 'Hugo Tovar - 31/10/2024' ?>
-<?php echo 'Un repositorio de código es como una carpeta personal tuya la cuál das acceso privado o público y que contiene un código web' ?>
+<h1><?php echo 'Hugo Tovar' ?></h1>
+<body>
+    <h2><?php echo 'Hugo Tovar' ?></h2>
+    <h3><?php echo 'Un repositorio de código es como una carpeta personal tuya la cuál das acceso privado o público y que contiene un código web' ?></h3>
+</body>
